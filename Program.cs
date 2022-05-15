6d9d454c47b0498c419e90fd6fe411c92e46aa98
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:26:13 -0300

    entrada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome");
+            string nomePessoa = Console.ReadLine();
+            
+
+            Console.Write("Qual é a sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Meu nome é: " + nomePessoa + " e minha idade é " + idade);
+            
+        }
+    }
+}
